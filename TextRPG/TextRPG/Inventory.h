62fc15cabf4ad8bccebb97038318cef0ad9b208f
youtaehoon <taehoongod@gmail.com>
2026-07-29 11:33:15 +0900

    KAN-41-인벤토리 해더 특정타입아이템 찾기 출력 제공

--- TextRPG/TextRPG/Inventory.h
+++ TextRPG/TextRPG/Inventory.h
@@ -12,6 +12,9 @@ public:
 	//인벤토리에 아이템 추가
 	void AddItem(const Item& item);
 	
-	//현재 인벤토리에 있는 아이템 목록 출력
-	void PrintInventory() const;
+	//인벤토리에 저장된 전체 아이템 목록을 반환
+	const std::vector<Item>& GetItems() const;
+
+	//특정 타입의 아이템 목록을 반환
+	std::vector<Item> GetItemsByType(ItemType type) const;
 };
