ad35dab6771eb12ba5816f902a65ce540bb40fd8
youtaehoon <taehoongod@gmail.com>
2026-07-29 23:13:48 +0900

    KAN-42-아이템헤더 장비 아이템 부위 클래스 생성

--- TextRPG/TextRPG/Item.h
+++ TextRPG/TextRPG/Item.h
@@ -7,13 +7,20 @@ enum class ItemType {
 	Equipment, //장비 아이템
 	Quest //퀘스트 아이템
 };
-
+//장비 부위
+enum class EquipmentType {
+	None, // 장비가 아닌 아이템
+	Weapon, // 무기
+	Armor // 방어구
+};
 //아이템 클래스
 class Item {
 private:
 	std::string name; //이름
 
 	ItemType type; //타입
+
+	EquipmentType equipmentType; //장비 유형
 
 	int hpRecovery;//hp회복
 	
@@ -25,12 +32,16 @@ private:
 
 	int quantity; // 아이템 수량
 
+	bool equipped; // 장비
+
 //아이템 생성자
 public:
-	Item(const std::string& name, ItemType type, int hpRecovery = 0, int mpRecovery = 0, int attackBonus = 0, int defenceBonus = 0, int quantity = 1);
+	Item(const std::string& name, ItemType type, EquipmentType equipmentType = EquipmentType::None,
+		int hpRecovery = 0, int mpRecovery = 0, int attackBonus = 0, int defenceBonus = 0, int quantity = 1);
 
 	std::string GetName() const;
 	ItemType GetType() const;
+	EquipmentType getEquipmentType() const;
 
 	int GetHpRecovery() const;
 	int GetMpRecovery() const;
@@ -44,5 +55,7 @@ public:
 	//아이템 수량 변경
 	void SetQuantity(int quantity);
 
+	bool IsEquipped() const;
+	void SetEquipped(bool equipped);
 };
 
