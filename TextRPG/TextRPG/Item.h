d0b29b799fdd7db1ecdd82ed163c54a640d6445f
youtaehoon <taehoongod@gmail.com>
2026-07-29 09:41:12 +0900

    KAN-41-아이템 헤더 아이템 클래스 생성

--- TextRPG/TextRPG/Item.h
+++ TextRPG/TextRPG/Item.h
@@ -8,3 +8,19 @@ enum class ItemType {
 	Quest //퀘스트 아이템
 };
 
+//아이템 클래스
+class Item {
+private:
+	std::string name; //이름
+	ItemType type; //타입
+	int effectAmount; //효과량
+
+//생성자
+public:
+	Item(const std::string& name, ItemType type, int effectAmount);
+
+	std::string GetName() const;
+	ItemType GetType() const;
+	int GetEffectAmount() const;
+
+};
