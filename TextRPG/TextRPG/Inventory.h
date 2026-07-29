a28a4ea81366b19987db0b1365ed92bb8f756e80
youtaehoon <taehoongod@gmail.com>
2026-07-29 10:18:32 +0900

    KAN-41-인벤토리 헤더 인벤토리 클래스 생성

--- TextRPG/TextRPG/Inventory.h
+++ TextRPG/TextRPG/Inventory.h
@@ -1,2 +1,17 @@
 #pragma once
-#include <string>
+#include <vector>
+#include "Item.h"
+
+//인벤토리 클래스
+class Inventory {
+private:
+	//인벤토리에 들어있는 아이템 목록
+	std::vector<Item> items;
+
+public:
+	//인벤토리에 아이템 추가
+	void AddItem(const Item& item);
+	
+	//현재 인벤토리에 있는 아이템 목록 출력
+	void PrintInventory() const;
+};
