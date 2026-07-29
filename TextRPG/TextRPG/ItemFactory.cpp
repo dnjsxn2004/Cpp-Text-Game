609d16cdbd26cfd34b450870775485691e6aa240
youtaehoon <taehoongod@gmail.com>
2026-07-29 21:50:56 +0900

    KAN-42-아이템팩토리 소스 소비아이템 생성

--- TextRPG/TextRPG/ItemFactory.cpp
+++ TextRPG/TextRPG/ItemFactory.cpp
@@ -11,3 +11,63 @@ Item ItemFactory::CreateCigarette(int quantity) {
 		quantity
 		);
 }
+
+Item ItemFactory::CreateAlcohol(int quantity) {
+	return Item(
+		"술",
+		ItemType::Consumable,
+		0, // HP 회복
+		20,  // MP 회복
+		0,  // 공격력 증가
+		0,  // 방어력 증가
+		quantity
+	);
+}
+
+Item ItemFactory::CreateDrug(int quantity) {
+	return Item(
+		"마약",
+		ItemType::Consumable,
+		40, // HP 회복
+		0,  // MP 회복
+		0,  // 공격력 증가
+		0,  // 방어력 증가
+		quantity
+	);
+}
+
+Item ItemFactory::CreateWhiskey(int quantity) {
+	return Item(
+		"위스키",
+		ItemType::Consumable,
+		0, // HP 회복
+		40,  // MP 회복
+		0,  // 공격력 증가
+		0,  // 방어력 증가
+		quantity
+	);
+}
+
+Item ItemFactory::CreateSyringe(int quantity) {
+	return Item(
+		"주사기",
+		ItemType::Consumable,
+		0, // HP 회복
+		0,  // MP 회복
+		10,  // 공격력 증가
+		0,  // 방어력 증가
+		quantity
+	);
+}
+
+Item ItemFactory::CreateCannabis(int quantity) {
+	return Item(
+		"대마초",
+		ItemType::Consumable,
+		40, // HP 회복
+		0,  // MP 회복
+		0,  // 공격력 증가
+		10,  // 방어력 증가
+		quantity
+	);
+}
