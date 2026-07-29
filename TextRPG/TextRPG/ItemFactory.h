e1697e2b20df34626514f0ffd5898c6d42b26e1d
youtaehoon <taehoongod@gmail.com>
2026-07-29 21:29:25 +0900

    KAN-42-아이템 팩토리 아이템 추가

--- TextRPG/TextRPG/ItemFactory.h
+++ TextRPG/TextRPG/ItemFactory.h
@@ -5,14 +5,31 @@
 class ItemFactory
 {
 public:
+
+    //소비 아이템
     //담배
-    static Item CreateCigarette();
+    static Item CreateCigarette(int quantity = 1);
     //술
-    static Item CreateAlcohol();
+    static Item CreateAlcohol(int quantity = 1);
     //마약
-    static Item CreateDrug();
+    static Item CreateDrug(int quantity = 1);
     //위스키
-    static Item CreateWhiskey();
+    static Item CreateWhiskey(int quantity = 1);
     //주사기
-    static Item CreateSyringe();
+    static Item CreateSyringe(int quantity = 1);
+    //대마초
+    static Item CreateCannabis(int quantity = 1);
+
+    //장비 아이템
+    //무기쪽
+    static Item CreateKnife();
+    static Item CreateBat();
+    static Item CreateKnuckle();
+    static Item CreateKatana();
+    //아머쪽
+    static Item CreateThomBrowne();
+    static Item CreateGucci();
+
+    //퀘스트 아이템
+    static Item CreateQuestkey();
 };
