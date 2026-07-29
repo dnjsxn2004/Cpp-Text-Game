0ef6ce02ba1e1a94a0eef4bd410554dcaeabca17
youtaehoon <taehoongod@gmail.com>
2026-07-29 10:07:04 +0900

    KAN-41-아이템 소스 getter 생성

--- TextRPG/TextRPG/Item.cpp
+++ TextRPG/TextRPG/Item.cpp
@@ -5,3 +5,16 @@ Item::Item(const std::string& name, ItemType type, int effectAmount)
 	:name(name), type(type), effectAmount(effectAmount) {
 
 }
+
+//아이템 이름 반환
+std::string Item::GetName() const {
+	return name;
+}
+//아이템 타입 반환
+ItemType Item::GetType() const {
+	return type;
+}
+//아이템 효과량 반환
+int Item::GetEffectAmount() const {
+	return effectAmount;
+}
