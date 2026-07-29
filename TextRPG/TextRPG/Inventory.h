69e42619122c81aa4d53d868707ea6093c524661
youtaehoon <taehoongod@gmail.com>
2026-07-29 23:29:42 +0900

    KAN-42-인벤토리 헤더 장비 장착 해제 설정

--- TextRPG/TextRPG/Inventory.h
+++ TextRPG/TextRPG/Inventory.h
@@ -11,6 +11,12 @@ private:
 	//인벤토리에 들어있는 아이템 목록
 	std::vector<Item> items;
 
+	//현재 장착한 무기 인덱스
+	int equippedWeaponIndex;
+
+	//현재 장착한 방어구 인덱스
+	int equippedArmorIndex;
+
 public:
 	//인벤토리에 아이템 추가
 	void AddItem(const Item& item);
@@ -24,6 +30,15 @@ public:
 	//아이템 사용
 	bool UseItem(int index, GameContext& context);
 
-	//장비 아이템
+	//장비 장착
 	bool EquipItem(int index, GameContext& context);
+
+	//무기 해제
+	bool UnequipWeapon(GameContext& context);
+
+	//방어구 해제
+	bool UnequipArmor(GameContext& context);
+
+	int GetEquippedWeaponIndex() const;
+	int GetEquippedArmorIndex() const;
 };
