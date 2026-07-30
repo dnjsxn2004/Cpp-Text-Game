5e49d8b7d527f0ba7a91dd1d951e8839109d9cb7
modi <lkjbn2@naver.com>
2026-07-30 19:36:14 +0900

    KAN-58

--- TextRPG/TextRPG/Monster.h
+++ TextRPG/TextRPG/Monster.h
@@ -18,12 +18,18 @@ private:
     int ExpReward;
     int GoldReward;
 
+    // [신규] 스킬 관련 변수 추가
+    string SkillName;
+    int SkillProbability;  // 0 ~ 100 사이의 스킬 발동 확률
+    float SkillMultiplier; // 스킬 발동 시 공격력 증폭 배수
+
+
 public:
     // 생성자
-    Monster(string Name, int Hp, int Attack, int Defense, int ExpReward, int GoldReward);
+    Monster(string Name, int Hp, int Attack, int Defense, int ExpReward, int GoldReward, string SkillName, int SkillProbability, float SkillMultiplier);
 
     // Getter 메서드 (전투 시스템에서 팀원이 가져다 쓸 함수들)
-    string getName() const { return Name; }
+    string GetName() const { return Name; }
     int GetHp() const { return Hp; }
     int GetMaxHp() const { return MaxHp; }
     int GetAttack() const { return Attack; }
@@ -35,6 +41,8 @@ public:
     int TakeDamage(int damage);
     bool IsDead() const;
     void PrintInfo() const;
+    // [신규] 확률적 스킬 발동을 포함한 최종 공격 데미지 계산 함수
+    int CalculateAttackDamage() const;
 };
 
 
@@ -42,6 +50,8 @@ public:
 class GameContext;
 
 // 랜덤 조우를 위한 몬스터 생성 도우미 함수 (레벨 스케일링 적용)
-Monster SpawnRandomMonster(int playerLevel);
+Monster SpawnRandomMonster(GameContext& Context);
+
+Monster SpawnFinalBoss(GameContext& Context);
 
 #endif
