e86e98ac392901d07e81eebbc7bdf1951c62a896
youtaehoon <taehoongod@gmail.com>
2026-07-31 16:50:32 +0900

    KAN-56-상점 카테고리 추가/아이템 구매 생성

--- TextRPG/TextRPG/Shop.cpp
+++ TextRPG/TextRPG/Shop.cpp
@@ -3,9 +3,11 @@
 #include "ItemFactory.h"
 #include "Player.h"
 #include "Inventory.h"
+#include "ConsolUI.h"
 
 Shop::Shop() {
-	//상점 기본 상품 등록
+	
+	//소비 아이템
 	products.push_back(ItemFactory::CreateCigarette());
 	products.push_back(ItemFactory::CreateAlcohol());
 	products.push_back(ItemFactory::CreateDrug());
@@ -13,11 +15,13 @@ Shop::Shop() {
 	products.push_back(ItemFactory::CreateSyringe());
 	products.push_back(ItemFactory::CreateCannabis());
 
+	//무기
 	products.push_back(ItemFactory::CreateKnife());
 	products.push_back(ItemFactory::CreateBat());
 	products.push_back(ItemFactory::CreateKnuckle());
 	products.push_back(ItemFactory::CreateKatana());
 
+	//아머
 	products.push_back(ItemFactory::CreateThomBrowne());
 	products.push_back(ItemFactory::CreateGucci());
 }
@@ -30,15 +34,48 @@ const std::vector<Item>& Shop::GetProducts() const {
 	return products;
 }
 
+std::vector<Item> Shop::GetProductsByCategory(ShopCategory category) const {
+	std::vector<Item>result;
+
+	for (const Item& item : products) {
+		
+		bool isMatched = false;
+
+		switch (category) {
+		case ShopCategory::Consumable:
+			isMatched =
+				item.GetType() == ItemType::Consumable;
+			    break;
+
+		case ShopCategory::Weapon:
+			isMatched =
+				item.GetType() == ItemType::Equipment && item.GetEquipmentType() == EquipmentType::Weapon;
+				break;
+
+		case ShopCategory::Armor:
+			isMatched =
+				item.GetType() == ItemType::Equipment && item.GetEquipmentType() == EquipmentType::Armor;
+			    break;
+		}
+
+		if (isMatched) {
+			result.push_back(item);
+		}
+	}
+	return result;
+}
+
 bool Shop::BuyItem(int productIndex, int quantity, GameContext& context) {
 	
-	//잘못된 상품 번호 검사
+	//잘못된 상품 인덱스 검사
 	if (productIndex < 0 || productIndex >= static_cast<int>(products.size())) {
+		ConsoleUI::PrintMessage("유효하지 않습니다.");
 		return false;
 	}
 
 	// 잘못된 구매 수량 검사
 	if (quantity <= 0) {
+		ConsoleUI::PrintMessage("잘못된 입력입니다.");
 		return false;
 	}
 
@@ -51,6 +88,7 @@ bool Shop::BuyItem(int productIndex, int quantity, GameContext& context) {
 
 	//장비는 한 번에 1개만 구매
 	if (product.GetType() == ItemType::Equipment && quantity != 1) {
+		ConsoleUI::PrintMessage("장비 아이템은 한개만 구매 가능합니다.");
 		return false;
 	}
 
@@ -60,6 +98,7 @@ bool Shop::BuyItem(int productIndex, int quantity, GameContext& context) {
 
 	//돈 부족 또는 잘못된 금액이면 구매 실패
 	if (player.GetGold() < totalPrice) {
+		ConsoleUI::PrintMessage("돈이 부족합니다");
 		return false;
 	}
 
@@ -75,4 +114,37 @@ bool Shop::BuyItem(int productIndex, int quantity, GameContext& context) {
 	context.GetInventory().AddItem(purchasedItem);
 
 	return true;
+}
+
+bool Shop::BuyItemByCategory(ShopCategory category, int categoryIndex, int quantity, GameContext& context) {
+	//인덱스 확인
+	if (categoryIndex < 0) {
+		return false;
+	}
+
+	//현재 카테고리 안에서 몇 번째 상품인지 세는 변수
+	int currentCategoryIndex = 0;
+
+	for (int productIndex = 0;
+		productIndex < static_cast<int>(products.size()); productIndex++) {
+
+		//products안의 실제 상품
+		const Item& product = products[productIndex];
+
+		//선택한 카테고리에 속하지 않는 상품이면 건너뜀
+		if (!IsMatchingCategory(product, category)) {
+			continue;
+		}
+
+		//선택한 번호가 같으면 상품 구매
+		if (currentCategoryIndex == categoryIndex) {
+
+			return BuyItem(productIndex, quantity, context);
+		}
+
+		//같은 카테고리 상품을 하나 찾았으니 번호 증가
+		currentCategoryIndex++;
+	}
+
+	return false;
 }
